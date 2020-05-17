47caa19295ee0cfe51c2dd6d673291e405b68a8c
Rolando Navarro <rolandonavarro@Rolandos-MacBook-Pro.local>
2020-05-17 14:04:50 -0500

    JS

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,58 @@
+// Getting reference to the dropdown menu ("Test Subject ID") to select the observation
+var dropdown = d3.select("#selDataset");
+
+// Getting reference of the Demographic Table 
+var demographic = d3.select("h3");
+
 // Use d3 to read the JSON file samples.json
-function samples() {
-    d3.json("samples.json").then(function(data) {
+(async function(){
+  var bdata = await d3.json("samples.json").catch(function(error) {
+    console.log(error);
+  });
+  console.log(bdata);
+
+  var names = [bdata.names]
+
+  names.forEach(function(something){
+    console.log(something)
+
+    var row = d3.select("#selDataset").append("options");
+
+    });
+  // d3.select("#selDataset")
+  //   .data(names)
+  //   .enter()
+  //   .append("li")
+  //   .text("1");
+})()
+
+
+
+// // Use d3 to read the JSON file samples.json
+// function samples() {
+//     d3.json("samples.json").then(function(data) {
         
-        var names = [data.names]
-        console.log(Object.values(names));
+//         var names = [data.names]
+//         console.log(Object.values(names));
 
-        var dropDown = d3.select("#selDataset").append
-    })
-}
-samples();
+//         var dropDown = d3.select("#selDataset").append
+//     })
+// }
+// samples();
 
 
-// Call updatePlotly() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", BacteriaInfo);
+// // Call updatePlotly() when a change takes place to the DOM
+// d3.selectAll("#selDataset").on("change", BacteriaInfo);
 
-// This function is called when a dropdown menu item is selected
-function BacteriaInfo() {
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
+// // This function is called when a dropdown menu item is selected
+// function BacteriaInfo() {
+//   // Use D3 to select the dropdown menu
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var dataset = dropdownMenu.property("value");
 
-}
+// }
 
-init();
+// // init();
 
 
