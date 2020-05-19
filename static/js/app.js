9ffd8e7e15c0181e4b4390db08397c50b6fd8f5e
Rolando Navarro <rolandonavarro@Rolandos-MacBook-Pro.local>
2020-05-18 22:15:32 -0500

    JS

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,6 @@
-// Getting reference to the dropdown menu ("Test Subject ID") to select the observation
-var dropdown = d3.select("select");
+const url = 'samples.json'
 
-// Getting reference of the Demographic Table 
-var demographic = d3.select(".panel-title");
-
-// Use d3 to read the JSON file samples.json
-(async function(){
-  var bdata = await d3.json("samples.json").catch(function(error) {
-    console.log(error);
+d3.json(url).then(function(data) {
+    console.log(data);
   });
-  console.log(bdata);
 
-  var names = [bdata.names]
-
-  names.forEach(function(something){
-    console.log(something);
-    var row = dropdown.append("options");
-
-    });
-
-})()
-
-
-// var row = dropdown.append("options").text(something);
